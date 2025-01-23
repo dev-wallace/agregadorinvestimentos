befca95292016b0d00423da8c6f28ab2aaa0e25e
yokaithefato <wallacewwc2015wwk@gmail.com>
2025-01-23 18:10:28 -0300

    add unit test for createUser method in UserService

--- src/test/java/dev/wallace/agregadorinvestimentos/service/UserServiceTest.java
+++ src/test/java/dev/wallace/agregadorinvestimentos/service/UserServiceTest.java
@@ -1,6 +1,12 @@
 package dev.wallace.agregadorinvestimentos.service;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 
+import java.time.Instant;
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +14,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import dev.wallace.agregadorinvestimentos.controller.CreateUserDto;
+import dev.wallace.agregadorinvestimentos.entity.User;
 import dev.wallace.agregadorinvestimentos.repository.UserRepository;
 
 
@@ -19,11 +27,36 @@ public class UserServiceTest {
     @InjectMocks UserService userService;
 
 
-        @Nested
-        class createUser{
-            @Test
-            void name(){
+    @Nested
+    class createUser {
+        @Test
+        @DisplayName("Should create a user with success")
+        void shouldCreateAUserWithSuccess() {
 
+                       var user = new User(
+                    UUID.randomUUID(),
+                    "username",
+                    "email@email.com",
+                    "password",
+                    Instant.now(),
+                    null
+            );
+
+            doReturn(user).when(userRepository).save(any());
+            var input = new CreateUserDto(
+                "username",
+                "email@email.com",
+                "123"
+        );
+
+            var output = userService.createUser(input);
+
+            assertNotNull(output);
+
+
+
+
+                    
             }
         }
 }
